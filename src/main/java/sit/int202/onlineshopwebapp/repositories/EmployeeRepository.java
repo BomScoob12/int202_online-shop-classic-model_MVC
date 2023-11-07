47d5d0b036061d90bcc418db22cabce684e2c5d2
BomScoob <sorawitkraukham@gmail.com>
2023-11-07 21:22:16 +0700

    Find Employee by office code

--- src/main/java/sit/int202/onlineshopwebapp/repositories/EmployeeRepository.java
+++ src/main/java/sit/int202/onlineshopwebapp/repositories/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package sit.int202.onlineshopwebapp.repositories;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
 import sit.int202.onlineshopwebapp.entities.Employee;
 import sit.int202.onlineshopwebapp.entities.Environment;
 
@@ -21,7 +22,8 @@ public class EmployeeRepository {
     }
 
     public List<Employee> findByOfficeCode(String officeCode){
-        getEntityManager().createNamedQuery("EMPLOYEE.FIND_BY_OFFICECODE").getResultList();
-        return null
+        Query query = getEntityManager().createNamedQuery("EMPLOYEE.FIND_BY_OFFICE_CODE");
+        query.setParameter("officeCode", officeCode);
+        return query.getResultList();
     }
 }
